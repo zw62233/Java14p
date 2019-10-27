b394f41d0db11ff8cc9312b73661f8230ff111f5
张伟 <869489612@qq.com>
2019-10-27 15:00:55 +0800

    1025 第一次学习 变量

---
+++ 1025/java1025.java
@@ -0,0 +1,15 @@
+public class java1025{
+    public static void main(String[] args){
+      System.out.println("HelloWorld\n");
+        short minshort = Short.MIN_VALUE;
+		short maxshort = Short.MAX_VALUE;
+		System.out.println(minshort);
+		System.out.println(maxshort);
+		long minlong = Long.MIN_VALUE;
+		long maxlong = Long.MAX_VALUE;
+		System.out.println(minlong);
+		System.out.println(maxlong);
+
+
+    }
+}
